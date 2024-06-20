2f58c987e82d5dd37f7f14f930df79a4e8218041
Stanislav Batyrgareev <62818625+S-DyX@users.noreply.github.com>
2024-06-20 20:13:47 +0300

    Сделал лонг

--- FileStorage.Contracts.Rest.Impl/FileStorage/GetBytesResult.cs
+++ FileStorage.Contracts.Rest.Impl/FileStorage/GetBytesResult.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FileStorage.Contracts.Rest.Impl.FileStorage
+﻿namespace FileStorage.Contracts.Rest.Impl.FileStorage
 {
 	internal class GetBytesResult
 	{
-		public int Offset { get; set; }
+		public long Offset { get; set; }
 		public int Size { get; set; }
 		public byte[] Bytes { get; set; }
 
